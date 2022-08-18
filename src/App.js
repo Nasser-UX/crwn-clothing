eccf285c02e282ab91b3a7b2f2d18a997e83e136
Ahmed Nasser <nasserart2017@gmail.com>
2022-08-18 23:54:29 +0300

    add new code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
  import React from "react";
  import './App.css';
 
+ import { Routes,Route } from "react-router-dom";
+
  import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-       <HomePage />
+      <Routes>
+        <Route  path='/' element={<HomePage />} />
+        <Route  path='hats' element={<HatsPage />} />
+
+      </Routes>
     </div>
   );
 }
--- src/components/menu-items/menu-item.component.jsx
+++ src/components/menu-items/menu-item.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) => (
+const MenuItem = ({title, imageUrl, size, linkUrl}) => (
     <div className={`${size} menu-item`}>
         <div className='background-image' style={{
                 backgroundImage: `url(${imageUrl})`
