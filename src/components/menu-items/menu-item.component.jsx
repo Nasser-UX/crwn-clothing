601b658c092d10f7167d009530af2e9cf56d27d5
Ahmed Nasser <nasserart2017@gmail.com>
2022-08-18 22:35:59 +0300

    start using react router

--- src/components/menu-items/menu-item.component.jsx
+++ src/components/menu-items/menu-item.component.jsx
@@ -10,8 +10,8 @@ const MenuItem = ({title, imageUrl, size}) => (
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
-        </div>
-    </div>
+        </div>  
+    </div>  
 )
 
 export default MenuItem;
