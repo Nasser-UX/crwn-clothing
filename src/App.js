3eb6560bd58fd3d1deeca2552aab18c694caf96e
Ahmed Nasser <nasserart2017@gmail.com>
2022-08-19 19:01:16 +0300

    create header component with its navigation links

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 
+import Header from "./components/header/header.component";
+
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
